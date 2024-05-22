1ae3b2afbe2288c4b9d08f934f90ccf389f7baed
Stuart.T.Williams <933105@cognizant.com>
2024-05-22 09:26:35 -0700

    Routing, 1

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # NodeJsPersonApi
+
 NodeJs REST API for Person
+
+## How to set up
+
+1. install deps
+
+`npm i`
+
+2. generate swagger
+
+`npm swagger-autogen`
+
+## Configure Port
+
+1. Configure port (powershell)
+
+`setx API_PORT 30090`
+
+Refresh the shell to load variable
+
+> OR
+
+2. Edit `launch.json`
+
+Change the `API_PORT`
+
+## Run site
+
+`npm start`
+
+## Unit tests
+
+1. Make sure JEST is installed
+
+`npm install --save-dev jest` (is part of the packages)
+
+2. Install it locally
+
+`npm install jest --global`
+
+3. Run tests with
+
+`jest`
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 "use strict";
 
-const express = require("express");
-const app = express();
 const port = process.env.API_PORT ?? 30090;
 
+const express = require("express");
+const app = express();
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
+
+const router = require("./routers/personRouter");
+
+app.use(express.json());
+app.use("/person", router.personRouter);
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.redirect("/doc");
 });
 
 app.listen(port, () => {

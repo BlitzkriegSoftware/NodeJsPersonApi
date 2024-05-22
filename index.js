a929db0d23bb192f60fac9b82d9ed835eff25743
Stuart.T.Williams <933105@cognizant.com>
2024-05-22 13:37:24 -0700

    Routing, 7

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 "use strict";
 
+// Environment Variables
 const port = process.env.API_PORT ?? 30090;
 
+// Express setup
 const express = require("express");
 const app = express();
-const cors = require("cors");
 
+// Swagger
+// https://github.com/scottie1984/swagger-ui-express/issues/120
 const swaggerUi = require("swagger-ui-express");
+
+const DisableTryItOutPlugin = function () {
+  return {
+    statePlugins: {
+      spec: {
+        wrapSelectors: {
+          allowTryItOutFor: () => () => false,
+        },
+      },
+    },
+  };
+};
+
+const options = {
+  swaggerOptions: {
+    plugins: [DisableTryItOutPlugin],
+  },
+};
+
 const swaggerFile = require("./swagger_output.json");
 
-const personRouter = require("./routers/personRouter");
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile, options));
 
 // https://dev.to/speaklouder/how-to-configure-cors-in-nodejs-with-express-11h
+const cors = require("cors");
 let corsOptions = {
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -20,20 +43,25 @@ let corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send(JSON.stringify(err));
 });
 
+// Enable JSON
 app.use(express.json());
 
+// Person methods
+const personRouter = require("./routers/personRouter");
 app.use("/", personRouter);
-app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+// Redirect to swagger
 app.get("/", (req, res) => {
   res.redirect("/doc");
 });
 
+// Start
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Person API listening on port ${port}`);
 });

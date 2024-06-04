43e4690575c3d798fa4cb3b148ab4df3061f121e
Stuart.T.Williams <933105@cognizant.com>
2024-06-04 14:51:00 -0700

    #4 - More InfoSec Checks, 3

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,22 @@
  * @ignore
  */
 
+const jsdoc = require('eslint-plugin-jsdoc');
+
 module.exports = [
   {
     ignores: [
-      '**/*.config.js',
-      'coverage/',
-      'docs/',
-      'node_modules/',
-      'scripts/',
+      '*.config.js',
+      'coverage/**',
+      'docs/**',
+      'node_modules/**',
+      'scripts/**',
     ],
+  },
+  {
+    plugins: {
+      jsdoc: jsdoc,
+    },
     rules: {
       'semi': 'off',
       'prefer-const': 'error',
